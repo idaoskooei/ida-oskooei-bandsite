7a27ed41d3f7afb14029159187e59084c0713347
idaoskooei <idaoskooei@gmail.com>
2024-06-28 17:08:09 -0400

    show shows page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,29 +1,29 @@
-const commentArray =[
+const commentArray = [
     {
-        name:"Victor Pinto",
-        date:"11/02/2023",
-        comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+        name: "Victor Pinto",
+        date: "11/02/2023",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
 
     },
 
     {
-        name:"Christina Cabrera",
-        date:"10/28/2023",
-        comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-       
+        name: "Christina Cabrera",
+        date: "10/28/2023",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+
     },
 
     {
-        name:"Isaac Tadesse",
-        date:"10/20/2023",
-        comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough"
+        name: "Isaac Tadesse",
+        date: "10/20/2023",
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough"
     },
 ]
 
 const commentList = document.querySelector(".post__items")
 
 function displayComments(commentArray) {
-    for (i = 0; i < commentArray.length; i=i+1){
+    for (i = 0; i < commentArray.length; i++){
 
         let listItems = document.createElement("li");
         listItems.classList.add("post__list-items", "post__list-items--top");
@@ -80,5 +80,4 @@ formCta.addEventListener("submit", (e) =>{
     document.querySelector(".comments__textbox--name").value = "";
     document.querySelector(".comments__textbox--comment").value = "";
 }
-
 )
