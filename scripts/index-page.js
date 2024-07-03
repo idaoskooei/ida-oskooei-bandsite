3e40e104b724e00e5abe782807f56fabe6413615
idaoskooei <idaoskooei@gmail.com>
2024-07-02 23:44:45 -0400

    post new comments and show 3 default comments calling api

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,84 +1,132 @@
 const url = "https:unit-2-project-api-25c1595833b2.herokuapp.com/";
 const apiKey = "3c6412bd-6a41-4e5b-b0ec-dd57c144bf86";
 
-const commentArray = [
-    {
-        name: "Victor Pinto",
-        date: "11/02/2023",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    },
+// const commentArray = [
+//     {
+//         name: "Victor Pinto",
+//         date: "11/02/2023",
+//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+//     },
 
-    {
-        name: "Christina Cabrera",
-        date: "10/28/2023",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
+//     {
+//         name: "Christina Cabrera",
+//         date: "10/28/2023",
+//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
 
-    {
-        name: "Isaac Tadesse",
-        date: "10/20/2023",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough"
-    },
-];
+//     {
+//         name: "Isaac Tadesse",
+//         date: "10/20/2023",
+//         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough"
+//     },
+// ];
 
 const commentList = document.querySelector(".post__items");
 
-function displayComments(commentArray) {
-    commentList.innerHTML = '';
+function getComments(){
+    axios
+        .get(url + "comments?api_key=" + apiKey)
+            .then(response => {
+                console.log(response.data);
+                let commentArray = response.data.sort((a,b) => {
+                    return b.timestamp - a.timestamp
+                });
+    
+                commentArray.forEach((comment) => {
+    
+                    let listItems = document.createElement("li");
+                    listItems.classList.add("post__list-items", "post__list-items--top");
+    
+                    let postHeader = document.createElement("div");
+                    postHeader.classList.add("post__header");
+    
+                    let headName = document.createElement("h3");
+                    headName.innerText = comment.name;
+    
+                    let headDate = document.createElement("p");
+                    headDate.classList.add("post__header--date");
+                    headDate.innerText = new Date(comment.timestamp).toDateString();
+    
+                    let text = document.createElement("p");
+                    text.classList.add("post__comment");
+                    text.innerText = comment.comment;
+    
+                    let avatar = document.createElement("img");
+                    avatar.classList.add("post__avatar");
+    
+    
+                    commentList.appendChild(listItems);
+                    listItems.appendChild(postHeader);
+                    postHeader.appendChild(headName);
+                    postHeader.appendChild(headDate);
+                    listItems.appendChild(text);
+                    listItems.appendChild(avatar);
+                });
+                
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    
+    getComments();
 
-    commentArray.forEach(comment => {
-        let listItem = document.createElement("li");
-        listItem.classList.add("post__list-items", "post__list-items--top");
+// function displayComments(commentArray) {
+//     commentList.innerHTML = '';
 
-        let postHeader = document.createElement("div");
-        postHeader.classList.add("post__header");
+//     commentArray.forEach(comment => {
+//         let listItem = document.createElement("li");
+//         listItem.classList.add("post__list-items", "post__list-items--top");
 
-        let headName = document.createElement("h3");
-        headName.innerText = comment.name;
+//         let postHeader = document.createElement("div");
+//         postHeader.classList.add("post__header");
 
-        let headDate = document.createElement("p");
-        headDate.classList.add("post__header--date");
-        headDate.innerText = formatTimeAgo(comment.date);
+//         let headName = document.createElement("h3");
+//         headName.innerText = comment.name;
 
-        let text = document.createElement("p");
-        text.classList.add("post__comment");
-        text.innerText = comment.comment;
+//         let headDate = document.createElement("p");
+//         headDate.classList.add("post__header--date");
+//         headDate.innerText = formatTimeAgo(comment.date);
 
-        let avatar = document.createElement("img");
-        avatar.classList.add("post__avatar");
+//         let text = document.createElement("p");
+//         text.classList.add("post__comment");
+//         text.innerText = comment.comment;
 
-        listItem.appendChild(postHeader);
-        postHeader.appendChild(headName);
-        postHeader.appendChild(headDate);
-        listItem.appendChild(text);
-        listItem.appendChild(avatar);
+//         let avatar = document.createElement("img");
+//         avatar.classList.add("post__avatar");
 
-        commentList.appendChild(listItem);
-    });
-}
+//         listItem.appendChild(postHeader);
+//         postHeader.appendChild(headName);
+//         postHeader.appendChild(headDate);
+//         listItem.appendChild(text);
+//         listItem.appendChild(avatar);
 
-// Display initial comments
-displayComments(commentArray);
+//         commentList.appendChild(listItem);
+//     });
+// }
 
-function formatTimeAgo(dateString) {
-    const datePosted = new Date(dateString);
-    const currentDate = new Date();
-    const timeDifference = currentDate.getTime() - datePosted.getTime();
-    const seconds = Math.floor(timeDifference / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
+// // Display initial comments
+// displayComments(commentArray);
 
-    if (days > 0) {
-        return `${days} day${days > 1 ? 's' : ''} ago`;
-    } else if (hours > 0) {
-        return `${hours} hour${hours > 1 ? 's' : ''} ago`;
-    } else if (minutes > 0) {
-        return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
-    } else {
-        return `${seconds} second${seconds !== 1 ? 's' : ''} ago`;
-    }
-}
+// function formatTimeAgo(dateString) {
+//     const datePosted = new Date(dateString);
+//     const currentDate = new Date();
+//     const timeDifference = currentDate.getTime() - datePosted.getTime();
+//     const seconds = Math.floor(timeDifference / 1000);
+//     const minutes = Math.floor(seconds / 60);
+//     const hours = Math.floor(minutes / 60);
+//     const days = Math.floor(hours / 24);
+
+//     if (days > 0) {
+//         return `${days} day${days > 1 ? 's' : ''} ago`;
+//     } else if (hours > 0) {
+//         return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+//     } else if (minutes > 0) {
+//         return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+//     } else {
+//         return `${seconds} second${seconds !== 1 ? 's' : ''} ago`;
+//     }
+// }
 
 let formCta = document.querySelector(".comments__textbox");
 
