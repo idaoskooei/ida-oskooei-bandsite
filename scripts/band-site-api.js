131180fd4f108818e217517e99b1e6c3932109e6
idaoskooei <idaoskooei@gmail.com>
2024-07-03 00:16:09 -0400

    add class and separate asyns functions

---
+++ scripts/band-site-api.js
@@ -0,0 +1,34 @@
+class BandSiteApi {
+    constructor(apiKey) {
+      this.apiKey = apiKey;
+      this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+    }
+  
+    async postComment(comment) {
+      try {
+        const response = await axios.post(`${this.baseUrl}comments?api_key=${this.apiKey}`, comment);
+        return response.data;
+      } catch (error) {
+        console.error('Error posting comment:', error);
+      }
+    }
+  
+    async getComments() {
+      try {
+        const response = await axios.get(`${this.baseUrl}comments?api_key=${this.apiKey}`);
+        return response.data.sort((a, b) => b.timestamp - a.timestamp);
+      } catch (error) {
+        console.error('Error getting comments:', error);
+      }
+    }
+  
+    async getShows() {
+      try {
+        const response = await axios.get(`${this.baseUrl}shows?api_key=${this.apiKey}`);
+        return response.data;
+      } catch (error) {
+        console.error('Error getting shows:', error);
+      }
+    }
+  }
+  
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,79 +1,71 @@
-const url = "https:unit-2-project-api-25c1595833b2.herokuapp.com/";
-const apiKey = "3c6412bd-6a41-4e5b-b0ec-dd57c144bf86";
+const bandSiteApi = new BandSiteApi("3c6412bd-6a41-4e5b-b0ec-dd57c144bf86");
 const commentList = document.querySelector(".post__items");
 
-function getComments(){
-    axios
-        .get(url + "comments?api_key=" + apiKey)
-            .then(response => {
-                console.log(response.data);
-                let commentArray = response.data.sort((a,b) => {
-                    return b.timestamp - a.timestamp
-                });
-    
-                commentArray.forEach((comment) => {
-    
-                    let listItems = document.createElement("li");
-                    listItems.classList.add("post__list-items", "post__list-items--top");
-    
-                    let postHeader = document.createElement("div");
-                    postHeader.classList.add("post__header");
-    
-                    let headName = document.createElement("h3");
-                    headName.innerText = comment.name;
-    
-                    let headDate = document.createElement("p");
-                    headDate.classList.add("post__header--date");
-                    headDate.innerText = new Date(comment.timestamp).toDateString();
-    
-                    let text = document.createElement("p");
-                    text.classList.add("post__comment");
-                    text.innerText = comment.comment;
-    
-                    let avatar = document.createElement("img");
-                    avatar.classList.add("post__avatar");
-    
-    
-                    commentList.appendChild(listItems);
-                    listItems.appendChild(postHeader);
-                    postHeader.appendChild(headName);
-                    postHeader.appendChild(headDate);
-                    listItems.appendChild(text);
-                    listItems.appendChild(avatar);
-                });
-                
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        }
-    
-    getComments();
+async function displayComments() {
+  try {
+    console.log("Fetching comments...");
+    const comments = await bandSiteApi.getComments();
+    console.log("Comments fetched:", comments);
+
+    commentList.innerHTML = "";
+    comments.forEach(comment => {
+      let listItems = document.createElement("li");
+      listItems.classList.add("post__list-items", "post__list-items--top");
+
+      let postHeader = document.createElement("div");
+      postHeader.classList.add("post__header");
+
+      let headName = document.createElement("h3");
+      headName.innerText = comment.name;
+
+      let headDate = document.createElement("p");
+      headDate.classList.add("post__header--date");
+      headDate.innerText = new Date(comment.timestamp).toDateString();
+
+      let text = document.createElement("p");
+      text.classList.add("post__comment");
+      text.innerText = comment.comment;
+
+      let avatar = document.createElement("img");
+      avatar.classList.add("post__avatar");
+
+      commentList.appendChild(listItems);
+      listItems.appendChild(postHeader);
+      postHeader.appendChild(headName);
+      postHeader.appendChild(headDate);
+      listItems.appendChild(text);
+      listItems.appendChild(avatar);
+    });
+  } catch (error) {
+    console.error("Error displaying comments:", error);
+  }
+}
+
+displayComments();
 
 let formCta = document.querySelector(".comments__textbox");
 
-formCta.addEventListener("submit", (e) => {
-    e.preventDefault();
+formCta.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    let nameText = e.target.nameText.value;
-    let commentText = e.target.commentText.value;
-    let newComment = {
-        name: nameText,
-        comment: commentText
+  let nameText = e.target.nameText.value;
+  let commentText = e.target.commentText.value;
+  let newComment = {
+    name: nameText,
+    comment: commentText
+  }
+
+  if (nameText && commentText) {
+    try {
+      console.log("Posting new comment...");
+      await bandSiteApi.postComment(newComment);
+      console.log("New comment posted");
+      displayComments();
+    } catch (error) {
+      console.error("Error posting new comment:", error);
     }
+  }
 
-    if (nameText && commentText) {
-        commentList.innerText = "";
-        axios
-        .post(url + "comments?api_key=" + apiKey, newComment)
-        .then(response=>{
-            getComments();
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }
-
-    document.querySelector(".comments__textbox--name").value = "";
-    document.querySelector(".comments__textbox--comment").value = "";
+  document.querySelector(".comments__textbox--name").value = "";
+  document.querySelector(".comments__textbox--comment").value = "";
 });
